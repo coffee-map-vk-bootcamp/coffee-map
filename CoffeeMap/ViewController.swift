37f42d9639925535a509f5531b3dc59045eadbeb
Golubev <golubev.da16@gmail.com>
2022-08-09 12:15:03 +0300

    Added configuration Firebase

---
+++ CoffeeMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoffeeMap
+//
+//  Created by Дмитрий Голубев on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor =
+    }
+
+
+}
+
