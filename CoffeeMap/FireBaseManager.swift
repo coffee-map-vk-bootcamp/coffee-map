5174915447558fcc319e454cdd2a1ca68aae2176
Matvei Borisov <bme15122000@gmail.com>
2022-08-20 00:01:56 +0300

    add user data request

--- CoffeeMap/FireBaseManager.swift
+++ CoffeeMap/FireBaseManager.swift
@@ -1,5 +1,5 @@
 //
-//  FireBase extencion.swift
+//  FireBaseManager.swift
 //  CoffeeMap
 //
 //  Created by Дмитрий Голубев on 11.08.2022.
@@ -8,46 +8,73 @@
 import Foundation
 import Firebase
 
-struct MockError: Error {
-    
+enum FireBaseError: Error {
+    case userNotLogin
+    case userNotFound
+    case receivedNilData
+    case dataParseError
 }
 
-final class FBService {
-    static let dataBase = Firestore.firestore()
+protocol NetworkManagerDescription {
+    func fetchUserData(completion: @escaping (Result<User, Error>) -> Void)
+}
+
+final class FBService: NetworkManagerDescription {
+    private let dataBase = Firestore.firestore()
     
-    static func fetchCoffeeShops(completion: @escaping (Result<[CoffeeShop], Error>) -> Void) {
-        dataBase.collection("coffeeShops1").addSnapshotListener { (querySnapshot, err) in
+    func addCoffeeShopsSubscription(completion: @escaping (Result<[CoffeeShop], Error>) -> Void) {
+        dataBase.collection("coffeeShops").addSnapshotListener { [weak self] (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
             } else {
-                guard let result = coffeeShops(from: querySnapshot) else { return }
+                guard let result = self?.coffeeShops(from: querySnapshot) else { return }
                 completion(.success(result))
             }
         }
+    }
+
+    func fetchUserData(completion: @escaping (Result<User, Error>) -> Void) {
+        guard let userId = getUserId() else {
+            completion(.failure(FireBaseError.userNotLogin))
+            return }
+
+        dataBase.collection("users").document(userId).getDocument { document, error in
+            guard error == nil else {
+                completion(.failure(FireBaseError.userNotFound))
+                return
+            }
+
+            guard let document = document else {
+                completion(.failure(FireBaseError.receivedNilData))
+                return
+            }
+
+            guard let result: User = try? ModelConverter.convert(from: document) else {
+                completion(.failure(FireBaseError.dataParseError))
+                return
+            }
+
+            completion(.success(result))
+        }
+    }
+
+    private func getUserId() -> String? {
+        return Auth.auth().currentUser?.uid
     }
 }
 
 private extension FBService {
     
-    static func coffeeShops(from snapshot: QuerySnapshot?) -> [CoffeeShop]? {
+    func coffeeShops(from snapshot: QuerySnapshot?) -> [CoffeeShop]? {
         return snapshot?.documents.compactMap {
             try? ModelConverter.convert(from: $0)
         }
-    }
-            
-    enum CoordinatesKeysNW: String {
-        case address
-        case latitude
-        case longitude
-        case dishes
-        case image
-        case name
     }
 }
 
 final class FBAuthService {
     
-    static func login(email: String, password: String, completion: @escaping (Result<String, Error>) -> Void) {
+    func login(email: String, password: String, completion: @escaping (Result<String, Error>) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) { _, error in
             if let error = error {
                 completion(.failure(error))
@@ -55,10 +82,9 @@ final class FBAuthService {
                 completion(.success("Success"))
             }
         }
-
     }
     
-    static func createUser(withEmail email: String, username: String, password: String, completion: @escaping (Result<String, Error>) -> Void) {
+    func createUser(withEmail email: String, username: String, password: String, completion: @escaping (Result<String, Error>) -> Void) {
         Auth.auth().createUser(withEmail: email, password: password, completion: { (_, err) in
             if let err = err {
                 print("Failed to create user:", err)
